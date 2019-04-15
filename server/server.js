e87335b6f9b542323f30c1bf2fbef8753ad3e343
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-04-15 20:54:10 +0100

    Added configurations

--- server/server.js
+++ server/server.js
@@ -40,7 +40,8 @@ firstUser.save().then(user => {
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
